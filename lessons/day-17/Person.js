2e6c34242684b7bc021e603043c937a42ca978a1
nguyenthaingocgiao <ngocgiaok18ufm@gmail.com>
2025-08-31 14:28:50 +0700

    push code

---
+++ lessons/day-17/Person.js
@@ -0,0 +1,35 @@
+class Person {
+
+    constructor(name, age){
+        //constructor: là người thợ
+        //để xét dc thuộc tính (property) đó -> dùng this.
+        this._name = name;
+        this._age = age;
+    }
+
+    //1 hàm bên trong 1 class - ko dùng từ "Method"
+    //Getters: is a function that return the CURRENT value of a property
+    getName(){
+        return this._name;
+    }
+    //Setters: reassign a property's value
+    setName(name){
+        this._name = name;
+    }
+
+}
+module.exports = Person; //xuất khẩu class
+
+//teo is an object that was created from Person Class Template
+let teo = new Person("Teo", 18);
+//Vay mượn từ khóa new. Khi gọi new, là đang gọi constructor trong class Person 
+//Tạo ra 1 new person tên Tèo
+
+//Break Encapsulation | Violation
+console.log(teo.name);
+teo.name = "Teo dep trai"
+// teo.setName("Teo Dep Trai");
+console.log(teo.name);
+
+
+
---
+++ lessons/day-17/SalaryController.js
@@ -0,0 +1,13 @@
+//FP - process data 
+
+class SalaryController{
+
+    //FP
+    getTotalSalary(employeeList){
+        let totalSalary = 0;
+        return totalSalary;
+    }
+
+}
+
+let salaryController = new SalaryController();
